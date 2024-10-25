660717eef5ae7148fe5131e3f1998457e34511b0
alexsanderlinux <alexsanderlinux@gmail.com>
2024-10-25 04:06:52 -0300

    feat: classe pessoa concluida

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,17 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Pessoa
     {
-        
+        public Pessoa() { }
+
+        public Pessoa(string nome, string sobrenome, int idade){
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Idade = idade;
+        }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
     }
 }
--- obj/Debug/net8.0/DesafioProjetoHospedagem.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesafioProjetoHospedagem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioProjetoHospedagem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58a94cd80486257eeacb71383120ad80f818070")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioProjetoHospedagem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioProjetoHospedagem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
